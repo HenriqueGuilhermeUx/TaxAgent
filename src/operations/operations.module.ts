42a680de26cf6656c3d05df787362967b02b45c1
HenriqueGuilhermeUx <henriquecampos66@gmail.com>
2026-08-14 22:47:16 -0300

    wire Prepared DPS into operations

--- src/operations/operations.module.ts
+++ src/operations/operations.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
 import { CertificatesModule } from '../certificates/certificates.module';
 import { MunicipalParametersModule } from '../municipal-parameters/municipal-parameters.module';
+import { PreparedDpsModule } from '../prepared-dps/prepared-dps.module';
 import { NfseNationalClient } from '../providers/nfse-national/nfse-national.client';
 import { SchemaRegistryModule } from '../schema-registry/schema-registry.module';
 import { TaxEngineModule } from '../tax-engine/tax-engine.module';
@@ -11,5 +12,5 @@ import { DpsPreflightService } from './dps-preflight.service';
 import { OperationsController } from './operations.controller';
 import { ReadinessService } from './readiness.service';
 
-@Module({ imports: [AuthModule, TenancyModule, CertificatesModule, SchemaRegistryModule, MunicipalParametersModule, TaxEngineModule, XmlEngineModule], controllers: [OperationsController], providers: [ReadinessService, DpsPreflightService, NfseNationalClient] })
+@Module({ imports: [AuthModule, TenancyModule, CertificatesModule, SchemaRegistryModule, MunicipalParametersModule, TaxEngineModule, XmlEngineModule, PreparedDpsModule], controllers: [OperationsController], providers: [ReadinessService, DpsPreflightService, NfseNationalClient] })
 export class OperationsModule {}
