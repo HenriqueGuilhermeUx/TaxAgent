2321ef018947627c2c3697060033d7cbdc62571e
HenriqueGuilhermeUx <henriquecampos66@gmail.com>
2026-08-14 22:47:47 -0300

    wire Prepared DPS into invoice issuance

--- src/invoices/invoices.module.ts
+++ src/invoices/invoices.module.ts
@@ -6,6 +6,7 @@ import { FiscalRouterService } from '../fiscal-core/fiscal-router.service';
 import { JobsModule } from '../jobs/jobs.module';
 import { FiscalLedgerService } from '../ledger/fiscal-ledger.service';
 import { MunicipalParametersModule } from '../municipal-parameters/municipal-parameters.module';
+import { PreparedDpsModule } from '../prepared-dps/prepared-dps.module';
 import { NfseNationalClient } from '../providers/nfse-national/nfse-national.client';
 import { NfseNationalProvider } from '../providers/nfse-national/nfse-national.provider';
 import { SchemaRegistryModule } from '../schema-registry/schema-registry.module';
@@ -19,5 +20,5 @@ import { InvoicesController } from './invoices.controller';
 import { InvoicesRepository } from './invoices.repository';
 import { InvoicesService } from './invoices.service';
 
-@Module({ imports: [AuthModule, JobsModule, TenancyModule, CertificatesModule, SchemaRegistryModule, MunicipalParametersModule, XmlEngineModule, DocumentsModule, WebhooksModule, TaxEngineModule], controllers: [InvoicesController], providers: [InvoicesService, InvoicesRepository, InvoiceWorkerService, CancellationWorkerService, FiscalRouterService, FiscalLedgerService, NfseNationalClient, NfseNationalProvider] })
+@Module({ imports: [AuthModule, JobsModule, TenancyModule, CertificatesModule, SchemaRegistryModule, MunicipalParametersModule, XmlEngineModule, DocumentsModule, WebhooksModule, TaxEngineModule, PreparedDpsModule], controllers: [InvoicesController], providers: [InvoicesService, InvoicesRepository, InvoiceWorkerService, CancellationWorkerService, FiscalRouterService, FiscalLedgerService, NfseNationalClient, NfseNationalProvider] })
 export class InvoicesModule {}
