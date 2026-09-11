375ff5da81e0647731370b53df6192458f54ff99
HenriqueGuilhermeUx <henriquecampos66@gmail.com>
2026-09-10 21:32:30 -0300

    feat(document-intake): wire provider inbox and webhooks

--- src/document-intake/document-intake.module.ts
+++ src/document-intake/document-intake.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
+import { FiscalInboxModule } from '../inbox/fiscal-inbox.module';
+import { WebhooksModule } from '../webhooks/webhooks.module';
+import { DocStructExtractorService } from './docstruct-extractor.service';
 import { DocumentIntakeController } from './document-intake.controller';
 import { DocumentIntakeService } from './document-intake.service';
+import { NativeDocumentExtractorService } from './native-document-extractor.service';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, FiscalInboxModule, WebhooksModule],
   controllers: [DocumentIntakeController],
-  providers: [DocumentIntakeService],
+  providers: [DocumentIntakeService, NativeDocumentExtractorService, DocStructExtractorService],
   exports: [DocumentIntakeService],
 })
 export class DocumentIntakeModule {}
