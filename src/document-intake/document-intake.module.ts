f9486701ea80520f933a8b3b55f706e0bf37804c
HenriqueGuilhermeUx <henriquecampos66@gmail.com>
2026-09-10 21:45:07 -0300

    feat(document-intake): wire encrypted file intake service

--- src/document-intake/document-intake.module.ts
+++ src/document-intake/document-intake.module.ts
@@ -4,13 +4,14 @@ import { FiscalInboxModule } from '../inbox/fiscal-inbox.module';
 import { WebhooksModule } from '../webhooks/webhooks.module';
 import { DocStructExtractorService } from './docstruct-extractor.service';
 import { DocumentIntakeController } from './document-intake.controller';
+import { DocumentIntakeFileService } from './document-intake-file.service';
 import { DocumentIntakeService } from './document-intake.service';
 import { NativeDocumentExtractorService } from './native-document-extractor.service';
 
 @Module({
   imports: [AuthModule, FiscalInboxModule, WebhooksModule],
   controllers: [DocumentIntakeController],
-  providers: [DocumentIntakeService, NativeDocumentExtractorService, DocStructExtractorService],
-  exports: [DocumentIntakeService],
+  providers: [DocumentIntakeService, DocumentIntakeFileService, NativeDocumentExtractorService, DocStructExtractorService],
+  exports: [DocumentIntakeService, DocumentIntakeFileService],
 })
 export class DocumentIntakeModule {}
