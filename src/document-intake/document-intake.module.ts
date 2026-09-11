c18a38b6672b735f9561808ed117cc64a782bdce
HenriqueGuilhermeUx <henriquecampos66@gmail.com>
2026-09-11 12:41:28 -0300

    feat(document-ocr): wire durable OCR worker

--- src/document-intake/document-intake.module.ts
+++ src/document-intake/document-intake.module.ts
@@ -1,20 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
 import { FiscalInboxModule } from '../inbox/fiscal-inbox.module';
+import { JobsModule } from '../jobs/jobs.module';
 import { WebhooksModule } from '../webhooks/webhooks.module';
 import { AzureDocumentOcrService } from './azure-document-ocr.service';
 import { DocStructExtractorService } from './docstruct-extractor.service';
 import { DocumentIntakeController } from './document-intake.controller';
 import { DocumentIntakeFileService } from './document-intake-file.service';
 import { DocumentIntakeService } from './document-intake.service';
+import { DocumentOcrWorkerService } from './document-ocr-worker.service';
 import { NativeDocumentExtractorService } from './native-document-extractor.service';
 import { PaymentMatchingService } from './payment-matching.service';
 import { ReconciliationCasesService } from './reconciliation-cases.service';
 
 @Module({
-  imports: [AuthModule, FiscalInboxModule, WebhooksModule],
+  imports: [AuthModule, FiscalInboxModule, JobsModule, WebhooksModule],
   controllers: [DocumentIntakeController],
-  providers: [DocumentIntakeService, DocumentIntakeFileService, NativeDocumentExtractorService, DocStructExtractorService, AzureDocumentOcrService, PaymentMatchingService, ReconciliationCasesService],
+  providers: [DocumentIntakeService, DocumentIntakeFileService, NativeDocumentExtractorService, DocStructExtractorService, AzureDocumentOcrService, DocumentOcrWorkerService, PaymentMatchingService, ReconciliationCasesService],
   exports: [DocumentIntakeService, DocumentIntakeFileService, AzureDocumentOcrService, PaymentMatchingService, ReconciliationCasesService],
 })
 export class DocumentIntakeModule {}
