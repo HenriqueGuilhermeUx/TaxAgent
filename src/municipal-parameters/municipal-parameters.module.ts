a54300222e2c1fd325ea07fd29a25daa83fc8c7d
HenriqueGuilhermeUx <henriquecampos66@gmail.com>
2026-09-22 15:19:22 -0300

    feat(router): export municipal capability service

--- src/municipal-parameters/municipal-parameters.module.ts
+++ src/municipal-parameters/municipal-parameters.module.ts
@@ -3,11 +3,12 @@ import { AuthModule } from '../auth/auth.module';
 import { MunicipalParametersClient } from './municipal-parameters.client';
 import { MunicipalParametersController } from './municipal-parameters.controller';
 import { NationalCoverageService } from './national-coverage.service';
+import { MunicipalCapabilityService } from './municipal-capability.service';
 
 @Module({
   imports: [AuthModule],
   controllers: [MunicipalParametersController],
-  providers: [MunicipalParametersClient, NationalCoverageService],
-  exports: [MunicipalParametersClient, NationalCoverageService],
+  providers: [MunicipalParametersClient, NationalCoverageService, MunicipalCapabilityService],
+  exports: [MunicipalParametersClient, NationalCoverageService, MunicipalCapabilityService],
 })
 export class MunicipalParametersModule {}
