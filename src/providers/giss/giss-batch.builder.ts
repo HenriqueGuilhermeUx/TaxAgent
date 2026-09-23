2e8bbd7af1b3d47dec93c8c40cb5cfbae750045d
HenriqueGuilhermeUx <henriquecampos66@gmail.com>
2026-09-22 21:40:44 -0300

    fix(giss): align RPS batch with current GISS ABRASF 2.04 contract

--- src/providers/giss/giss-batch.builder.ts
+++ src/providers/giss/giss-batch.builder.ts
@@ -14,5 +14,6 @@ export function buildAbrasfLoteRps(input: GissBatchInput): string {
   const cnpj = digits(input.providerTaxId);
   if (cnpj.length !== 14) throw new FiscalEngineError('TA_GISS_PROVIDER_CNPJ_INVALID', 'GISS batch requires a 14-digit provider CNPJ', false);
   if (!input.rpsXml.includes('<InfDeclaracaoPrestacaoServico')) throw new FiscalEngineError('TA_GISS_RPS_XML_INVALID', 'GISS batch requires a validated RPS XML payload', false);
-  return `<EnviarLoteRpsSincronoEnvio xmlns="http://www.abrasf.org.br/nfse.xsd"><LoteRps Id="LOTE${esc(input.batchNumber)}" versao="2.04"><NumeroLote>${esc(input.batchNumber)}</NumeroLote><CpfCnpj><Cnpj>${cnpj}</Cnpj></CpfCnpj>${input.municipalRegistration ? `<InscricaoMunicipal>${esc(input.municipalRegistration)}</InscricaoMunicipal>` : ''}<QuantidadeRps>1</QuantidadeRps><ListaRps>${input.rpsXml}</ListaRps></LoteRps></EnviarLoteRpsSincronoEnvio>`;
+  const prestador = `<Prestador><CpfCnpj><Cnpj>${cnpj}</Cnpj></CpfCnpj>${input.municipalRegistration ? `<InscricaoMunicipal>${esc(input.municipalRegistration)}</InscricaoMunicipal>` : ''}</Prestador>`;
+  return `<EnviarLoteRpsEnvio xmlns="http://www.abrasf.org.br/nfse.xsd"><LoteRps Id="LOTE${esc(input.batchNumber)}" versao="2.04"><NumeroLote>${esc(input.batchNumber)}</NumeroLote>${prestador}<QuantidadeRps>1</QuantidadeRps><ListaRps>${input.rpsXml}</ListaRps></LoteRps></EnviarLoteRpsEnvio>`;
 }
