57a37031ec828ceb8ba26a1e74f78af08fe6138d
HenriqueGuilhermeUx <henriquecampos66@gmail.com>
2026-09-23 16:32:48 -0300

    feat(giss): wire audited reconciliation query service

--- src/operations/operations.module.ts
+++ src/operations/operations.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
 import { CertificatesModule } from '../certificates/certificates.module';
+import { DocumentsModule } from '../documents/documents.module';
+import { FiscalLedgerService } from '../ledger/fiscal-ledger.service';
 import { MunicipalParametersModule } from '../municipal-parameters/municipal-parameters.module';
 import { PreparedDpsModule } from '../prepared-dps/prepared-dps.module';
+import { GissArtifactsService } from '../providers/giss/giss-artifacts.service';
 import { GissClient } from '../providers/giss/giss.client';
 import { NfseNationalClient } from '../providers/nfse-national/nfse-national.client';
 import { SchemaRegistryModule } from '../schema-registry/schema-registry.module';
@@ -10,6 +13,7 @@ import { TaxEngineModule } from '../tax-engine/tax-engine.module';
 import { TenancyModule } from '../tenancy/tenancy.module';
 import { XmlEngineModule } from '../xml-engine/xml-engine.module';
 import { DpsPreflightService } from './dps-preflight.service';
+import { GissQueryExecutionService } from './giss-query-execution.service';
 import { GissWsdlDiagnosticService } from './giss-wsdl-diagnostic.service';
 import { MunicipalityScenarioController } from './municipality-scenario.controller';
 import { MunicipalityScenarioService } from './municipality-scenario.service';
@@ -18,9 +22,9 @@ import { OperationsController } from './operations.controller';
 import { ReadinessService } from './readiness.service';
 
 @Module({
-  imports: [AuthModule, TenancyModule, CertificatesModule, SchemaRegistryModule, MunicipalParametersModule, TaxEngineModule, XmlEngineModule, PreparedDpsModule],
+  imports: [AuthModule, TenancyModule, CertificatesModule, SchemaRegistryModule, MunicipalParametersModule, TaxEngineModule, XmlEngineModule, PreparedDpsModule, DocumentsModule],
   controllers: [OperationsController, MunicipalityScenarioController],
-  providers: [ReadinessService, DpsPreflightService, NoA1HomologationService, NfseNationalClient, GissClient, GissWsdlDiagnosticService, MunicipalityScenarioService],
+  providers: [ReadinessService, DpsPreflightService, NoA1HomologationService, NfseNationalClient, GissClient, GissWsdlDiagnosticService, GissQueryExecutionService, GissArtifactsService, FiscalLedgerService, MunicipalityScenarioService],
   exports: [ReadinessService, NoA1HomologationService],
 })
 export class OperationsModule {}
