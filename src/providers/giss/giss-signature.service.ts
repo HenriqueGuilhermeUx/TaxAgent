816a69ff61b5dcbe63c1414052045a819bcb2b39
HenriqueGuilhermeUx <henriquecampos66@gmail.com>
2026-09-22 21:10:14 -0300

    feat(giss): support RPS and batch XMLDSig signing

--- src/providers/giss/giss-signature.service.ts
+++ src/providers/giss/giss-signature.service.ts
@@ -12,4 +12,14 @@ export class GissSignatureService {
     if (!match) throw new FiscalEngineError('TA_GISS_RPS_ID_REQUIRED', 'ABRASF RPS must contain InfDeclaracaoPrestacaoServico/@Id before XMLDSig signing', false);
     return this.signatures.sign(xml, match[1], 'InfDeclaracaoPrestacaoServico', material);
   }
+
+  signBatch(xml: string, material: CertificateMaterial): string {
+    const match = xml.match(/<LoteRps\s+Id="([^"]+)"/);
+    if (!match) throw new FiscalEngineError('TA_GISS_BATCH_ID_REQUIRED', 'ABRASF batch must contain LoteRps/@Id before XMLDSig signing', false);
+    return this.signatures.sign(xml, match[1], 'LoteRps', material);
+  }
+
+  signRpsAndBatch(xml: string, material: CertificateMaterial): string {
+    return this.signBatch(this.signRps(xml, material), material);
+  }
 }
