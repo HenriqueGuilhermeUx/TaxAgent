5106943aa551ada91c0b03ec9c4dee4274f2a5a1
HenriqueGuilhermeUx <henriquecampos66@gmail.com>
2026-09-23 15:46:14 -0300

    fix(giss): use verified 2.04 query namespaces

--- src/providers/giss/giss-query.builder.ts
+++ src/providers/giss/giss-query.builder.ts
@@ -1,6 +1,9 @@
 import { FiscalEngineError } from '../../fiscal-core/fiscal-engine.error';
+import { GISS_TYPES_NAMESPACE } from './giss-header.builder';
 
 export interface GissRpsQueryInput { providerTaxId: string; municipalRegistration?: string | null; number: string; series: string }
+
+export const GISS_QUERY_NAMESPACE = 'http://www.giss.com.br/consultar-nfse-rps-envio-v2_04.xsd' as const;
 
 const digits = (value: string) => value.replace(/\D/g, '');
 const esc = (value: unknown) => String(value ?? '').replaceAll('&', '&amp;').replaceAll('<', '&lt;').replaceAll('>', '&gt;').replaceAll('"', '&quot;').replaceAll("'", '&apos;');
@@ -9,5 +12,5 @@ export function buildConsultarNfsePorRps(input: GissRpsQueryInput): string {
   const cnpj = digits(input.providerTaxId);
   if (cnpj.length !== 14) throw new FiscalEngineError('TA_GISS_PROVIDER_CNPJ_INVALID', 'GISS RPS query requires a 14-digit provider CNPJ', false);
   if (!input.number || !input.series) throw new FiscalEngineError('TA_GISS_RPS_IDENTITY_REQUIRED', 'GISS RPS query requires number and series', false);
-  return `<ConsultarNfseRpsEnvio xmlns="http://www.abrasf.org.br/nfse.xsd"><IdentificacaoRps><Numero>${esc(input.number)}</Numero><Serie>${esc(input.series)}</Serie><Tipo>1</Tipo></IdentificacaoRps><Prestador><CpfCnpj><Cnpj>${cnpj}</Cnpj></CpfCnpj>${input.municipalRegistration ? `<InscricaoMunicipal>${esc(input.municipalRegistration)}</InscricaoMunicipal>` : ''}</Prestador></ConsultarNfseRpsEnvio>`;
+  return `<ConsultarNfseRpsEnvio xmlns="${GISS_QUERY_NAMESPACE}" xmlns:tipos="${GISS_TYPES_NAMESPACE}"><IdentificacaoRps><tipos:Numero>${esc(input.number)}</tipos:Numero><tipos:Serie>${esc(input.series)}</tipos:Serie><tipos:Tipo>1</tipos:Tipo></IdentificacaoRps><Prestador><tipos:CpfCnpj><tipos:Cnpj>${cnpj}</tipos:Cnpj></tipos:CpfCnpj>${input.municipalRegistration ? `<tipos:InscricaoMunicipal>${esc(input.municipalRegistration)}</tipos:InscricaoMunicipal>` : ''}</Prestador></ConsultarNfseRpsEnvio>`;
 }
