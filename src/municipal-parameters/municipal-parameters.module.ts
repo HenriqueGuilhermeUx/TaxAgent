038fd26c6259469bdc05b363bd8ba044d98b8cf5
HenriqueGuilhermeUx <henriquecampos66@gmail.com>
2026-09-24 16:11:20 -0300

    feat(coverage): wire metropolitan matrix services

--- src/municipal-parameters/municipal-parameters.module.ts
+++ src/municipal-parameters/municipal-parameters.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
+import { IbgeLocationsClient } from './ibge-locations.client';
+import { MetropolitanCoverageService } from './metropolitan-coverage.service';
 import { MunicipalParametersClient } from './municipal-parameters.client';
 import { MunicipalParametersController } from './municipal-parameters.controller';
 import { NationalCoverageService } from './national-coverage.service';
@@ -8,7 +10,7 @@ import { MunicipalCapabilityService } from './municipal-capability.service';
 @Module({
   imports: [AuthModule],
   controllers: [MunicipalParametersController],
-  providers: [MunicipalParametersClient, NationalCoverageService, MunicipalCapabilityService],
-  exports: [MunicipalParametersClient, NationalCoverageService, MunicipalCapabilityService],
+  providers: [MunicipalParametersClient, NationalCoverageService, MunicipalCapabilityService, IbgeLocationsClient, MetropolitanCoverageService],
+  exports: [MunicipalParametersClient, NationalCoverageService, MunicipalCapabilityService, IbgeLocationsClient, MetropolitanCoverageService],
 })
 export class MunicipalParametersModule {}
